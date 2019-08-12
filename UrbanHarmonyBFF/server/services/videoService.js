b931bc76f32dcdebdcd1164db4891a2b930712fe
abdelazizelsaman <14p1064@eng.asu.edu.eg>
2019-08-12 22:33:46 +0200

    Fixed issue #24

--- UrbanHarmonyBFF/server/services/videoService.js
+++ UrbanHarmonyBFF/server/services/videoService.js
@@ -5,16 +5,49 @@ const url = localConfig.CMS_URL || process.env.CMS_URL;
 
 class VideosService {
 
+    getAuthToken(callback){
+        const user = localConfig.USER || process.env.USER;
+        const secert = localConfig.SECERT || process.env.SECERT;
+        
+        let authPath = url + 'auth/local/';
+        let config = {
+            method: 'POST',
+            body: JSON.stringify({
+                "identifier": `${user}`,
+                "password": `${secert}`
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+        fetch(authPath, config)
+        .then(function(res){
+            return res.json();
+        })
+        .then(function(json){
+            callback(json.jwt);
+        })
+    }
+
     getAllVideos(callback) {
-        fetch(url + 'videos')
+        this.getAuthToken(function(token){
+            fetch(url + 'videos', {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": `Bearer ${token}`
+                }
+            })
             .then(function (response) {
                 return response.json();
             })
             .then(function (myJson) {
                 let expression = jsonata('$.{"id":_id,"channel":Channel,"title":ProgramName,"guests":GuestNames,"date":date,"description":Description,"url":URL,"moreDetails":MoreDetails}');
                 let videosObject = expression.evaluate(myJson);
+                
                 callback(videosObject);
             });
+        })
+        
     }
 }
 
