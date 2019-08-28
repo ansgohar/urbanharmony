f0c283a076e4bf09f9f8292084ddb02dbbfd13c9
abdelazizelsaman <14p1064@eng.asu.edu.eg>
2019-08-28 17:39:48 +0200

    Fixed issue #60

--- UrbanHarmonyBFF/client/app/components/conference/conferences.jsx
+++ UrbanHarmonyBFF/client/app/components/conference/conferences.jsx
@@ -2,53 +2,111 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ReactMarkdown from 'react-markdown';
 import { getConferences } from '../../actions/index.js';
-
+import * as fetch from 'node-fetch';
 
 class Conferences extends React.Component {
     constructor(props) {
         super(props);
-        this.getConferences();
+        this.state={
+            conferencesList:[]
+        }
+
     }
 
 
     getConferences() {
-        fetch('/conferences', {
-            method: 'GET'
-        }).then(res => {
-            console.log(res);
-            return res.json()
-        }
 
-        ).then(data => this.props.dispatch(getConferences(data))
-        );
+        let url = 'http://localhost:3000/conferencesList';
+        let options = {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json'
+            }
+        };
+        
+        fetch(url, options)
+            .then(res => res.json())
+            .then(c =>{
+                this.setState(prevState =>{
+                    return{
+                        conferencesList: c
+                    }
+                    
+                })
+            })
+            .catch(err => console.log(err));
+    }
 
-    
+    componentDidMount(){
+        this.getConferences();
     }
 
     render() {
-        console.log(this.props)
-        return (
+
+        function setConference(conference){
+
+            let conferenceType = (conference.type === "Seminar" ? "ندوة" : "مؤتمر داخلي")
+
+            var event = new Date (conference.date)
+
+            var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+            let conferenceDate = event.toLocaleDateString('ar-EG', options);
+
+            return <TableRow title={conference.title} conferenceType={conferenceType} key={conference.id} conferenceDate={conferenceDate} />
+        }
+
+       
+
+        return(
             <div id="project-details" className="row project-details">
                 <div className="col-xs-12 no-padding greyish-background">
                     <h2 className="sec-h2 add-padding">الندوات والمؤتمرات</h2>
                     <div className="add-margin">
-                        <table className="default-table no-border-table">
-                            <tr className="table-row">
-                                <td className="table-data table-header">عنوان المؤتمر </td>
-                                {/* <td className="table-data">{this.props.record.channel}</td> */}
-                            </tr>
-
-                            <tr className="table-row">
-                                <td className="table-data table-header"> تاريخ المؤتمر </td>
-                                {/* <td className="table-data">{this.props.record.title}</td> */}
-                            </tr>
-                        </table>
+                        {this.state.conferencesList.map(setConference)}
                     </div>
                 </div>
             </div>
+        )
+        
+
+
+    }
+}
+
+class TableRow extends React.Component{
+
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+
+        console.log(this.props);
+
+        
+        
+
+
+        return(
+            <table className="default-table no-border-table">
+                            <tr className="table-row">
+                                <td className="table-data table-header" >عنوان المؤتمر </td>
+                                {this.props.title}
+                            </tr>
+
+                            <tr className="table-row">
+                                <td className="table-data table-header" > تاريخ المؤتمر </td>
+                                {this.props.conferenceDate.toString()}
+                            </tr>
+
+                                <tr className="table-row">
+                                <td className="table-data table-header"> نوع المؤتمر </td>
+                                {this.props.conferenceType}
+                            </tr>
+                            
+                        </table>
         );
-
-
     }
 }
 
--- UrbanHarmonyBFF/server/routers/test.js
+++ UrbanHarmonyBFF/server/routers/test.js
@@ -1,15 +1,30 @@
-var express = require('express');
+'use strict';
 
-module.exports = function(app) {
-  var router = express.Router();
+const express = require('express');
+const fetch = require('node-fetch');
 
-  router.get('/', function (req, res, next) {
-    res.json({status: 'Nada'});
+module.exports = function(app){
+
+  const router = express.Router();
+
+  router.get('/', (req, res) => {
+
+    let url = 'http://localhost:3000/conferencesList';
+    let options = {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json'
+      }
+    };
+
+    console.log(options);
+
+    fetch(url, options)
+      .then(res => res.json())
+      .then(body => res.json(body))
+      .catch(err => console.err);
+
   });
 
-  
-  app.use("/test", router);
+  app.use('/test', router);
 }
-
-
-
