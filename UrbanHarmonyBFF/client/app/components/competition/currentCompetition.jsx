d14278fdd366a423388f9e28feebc706b80f7813
Omar-Handouk <omar.handouk@yandex.com>
2020-01-08 18:19:18 +0200

    Fixed current competitions not loading

--- UrbanHarmonyBFF/client/app/components/competition/currentCompetition.jsx
+++ UrbanHarmonyBFF/client/app/components/competition/currentCompetition.jsx
@@ -17,33 +17,46 @@ class CurrentCompetition extends React.Component {
     componentDidMount() {
         this.competitionOfTheMonth();
     }
+	
     render() {
 
         let date = new Date();
-
-        let currentComps = this.props.competition.map(value => {
-            if ((new Date()) <= (new Date(value.deadline))){
-                return <React.Fragment>
-                    <div className="cardText col-xs-12">
-                        <h2> {value.title}</h2>
-                        <ReactMarkdown source={value.description} />
-                        <ReactMarkdown source={value.rules} />
-                        <p>{value.awards}</p>
-                        <ReactMarkdown source={value.judges} />
-                    </div>
-                    <hr></hr>
-                </React.Fragment>;
-            }
-            
-        });
-
+        
+        let currentCompetition = this.props.competition;
+        console.info(currentCompetition);
+        if (currentCompetition && date <= (new Date(currentCompetition.deadline))) {
         return (
-            <div>
-                {currentComps}
-            </div>
-        );
-    }
+        <div className="cardText col-xs-12">
+            <h2> {currentCompetition.title}</h2>
+            <ReactMarkdown source={currentCompetition.description} />
+            <ReactMarkdown source={currentCompetition.rules} />
+            <p>{currentCompetition.awards}</p>
+            <ReactMarkdown source={currentCompetition.judges} />
+            <a href={`/competitionInfo?competition=${currentCompetition.id}`}>للمزيد من التفاصيل اضغط هنا</a>
+        </div>
+        )
+        }
 
+		/*if (this.props.competition){
+			if ((new Date()) <= (new Date(this.props.competition.deadline))){
+                return (
+				<div>
+                    <div className="cardText col-xs-12">
+                        <h2> {this.props.competition.title}</h2>
+                        <ReactMarkdown source={this.props.competition.description} />
+                        <ReactMarkdown source={this.props.competition.rules} />
+                        <p>{this.props.competition.awards}</p>
+                        <ReactMarkdown source={this.props.competition.judges} />
+                    </div>
+                </div>
+					   );
+		    }
+        } else {
+            return (<div><h1>لا يوجد مسابقات حالية</h1></div>)
+        }*/
+
+        return (<h1>لا يوجد مسابقات حاليا</h1>)
+	}
 }
 
 export default connect()(CurrentCompetition);
