d84ae7c74cbe274ce508691485b864dc6872222d
Omar-Handouk <omar.handouk@yandex.com>
2020-03-08 13:24:18 +0200

    Change logs: - Added search functionality to studies

--- UrbanHarmonyBFF/server/routers/studies.js
+++ UrbanHarmonyBFF/server/routers/studies.js
@@ -7,7 +7,7 @@ module.exports = (app) => {
 
 	router.get('/', (req, res) => {
 		try {
-			studies.getStudies(null, data => {
+			studies.getStudies(data => {
 				if (data === undefined) {
 					return res.status(500).json({error: 'Internal server error'});				
 				}
@@ -22,6 +22,26 @@ module.exports = (app) => {
 			console.error(err.message);
 			res.status(500).json({error: 'Internal server error'});
 		}	
+	});
+
+	router.post('/filter', (req, res) => {
+		try {
+			studies.filterStudies(req.body, data => {
+				if (data === undefined) {
+					return res.status(500).json({error: 'Internal server error'});				
+				}
+
+				if (Array.isArray(data)) {
+					res.status(200).json(data);
+				} else {
+					res.status(200).json([data]);
+				}
+			});
+
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).json({error: 'Internal server error'});
+		}
 	});
 	
 	app.use('/studies', router);	
--- UrbanHarmonyBFF/server/services/studiesService.js
+++ UrbanHarmonyBFF/server/services/studiesService.js
@@ -7,7 +7,7 @@ const auth = require('../services/service-manager').get('auth-service');
 const url = config.CMS_URL || process.env.CMS_URL;
 
 class StudiesService {
-	getStudies(query, callback) {
+	getStudies(callback) {
 		auth.getAuthToken((token) => {
 			const path = `${url}studies?_limit=100000`;
 			const request = {
@@ -23,6 +23,27 @@ class StudiesService {
 				.then(response => response.json())
 				.then(body => callback(body))
 				.catch(err => console.error(err));
+		});
+	}
+
+	filterStudies(query, callback) {
+		auth.getAuthToken(token => {
+			const path = `${url}graphql`;
+			const request = {
+				method: 'POST',
+				body: JSON.stringify(query),
+				headers: {
+					'Content-Type': 'application/json',
+					'Accept': 'application/json',
+					'User-Agent': 'NodeJS',
+					'Authorization': `Bearer ${token}`
+				}
+			}
+			
+			fetch(path, request)
+			.then(response => response.json())
+			.then(body => callback(body))
+			.catch(err => console.error(err));
 		});
 	}
 }
